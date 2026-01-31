99aa825fb6262680e74b9eaa8f7ee1b13b980934
Triveni Balla <your-github-triveniballa01@gmail.com>
2026-01-31 20:35:26 +0530

    Updated the trip and user controllers

---
+++ root/controllers/trip.controller.js
@@ -0,0 +1,54 @@
+const supabase = require("../config/supabase");
+const createTrip=async(req , res)=>{
+    try{
+        const{customer_id, vehicle_id, distance_km, passengers}=req.body;
+        const{data:vehicle}=await supabase
+        .from("vehicles")
+        .select("*")
+        .eq("id",vehicle_id)
+        .single();
+
+        if(!vehicle.isAvailable){
+            return res.status(400).json({message:"Vehicle not available"});
+        }
+        if (passengers> vehicle.allowed_passengers){
+            return res.status(400).json({message:"Passengers exceed limit"});
+        }
+        await supabase.from("trips").insert([{
+            customer_id,vehicle_id,distance_km,passengers
+        }]);
+        await supabase
+        .from ("vehicles")
+        .update({isAvailable:false})
+        .eq("id", vehicle_id);
+        res.status(201).json({message:"Trip created"});
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
+const endTrip=async(req , res)=>{
+    try{
+        const{data:tripId}=req.params;
+        const{data:trip}=await supabase
+        .from("trips")
+        .select("*")
+        .eq("id",tripId)
+        .single();
+
+        const cost=trip.distance_km *vehicle.rate_per_km;
+        await supabase
+        .from("trips")
+        .update({isCompleted:true,tripCost:cost})
+        .eq("id",tripId);
+
+        await supabase
+        .from("vehicles")
+        .update({isAvailable:true})
+        .eq("id",trip.vehicle_id);
+
+        res.json({message:"Trip ended",tripCost:cost});
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
+module.exports={createTrip,endTrip};
---
+++ root/controllers/user.controller.js
@@ -0,0 +1,20 @@
+const supabase=require("../config/supabase");
+const signupUser=async (req , res)=>{
+    try{
+        const {name,email,role}=req.body;
+
+        if(!name || !email || !role){
+            return res.status(400).json ({message:"All fields required"});
+        }
+        const{error}=await supabase
+        .from("users")
+        .insert([{name,email,role}]);
+
+        if(error) return res.status(400).json({error:error.message});
+        res.status(201).json({message:"User created successfully"});
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+};
+
+module.exports ={signupUser};
